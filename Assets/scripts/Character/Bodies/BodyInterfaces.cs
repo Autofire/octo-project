6da4366dcd9488d5a9435c21e19e25447539d783
Autofire <Autofire@users.noreply.github.com>
2019-06-29 21:41:23 -0700

    Worked on brain/body system.

---
+++ Assets/scripts/Character/Bodies/BodyInterfaces.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Characters.Bodies.Interfaces {
+
+	/// <summary>
+	/// The base type of all body classes. Implementing this means nothing on its own,
+	/// but it is an indication of more specific body interfaces.
+	/// </summary>
+	public interface IBody { }
+
+
+	/// <summary>
+	/// Implementors can move around with a constant speed in any direction.
+	/// </summary>
+	public interface IWalk : IBody {
+
+		/// <summary>
+		/// The maximum walk speed.
+		/// </summary>
+		float WalkSpeed { get; }
+
+		/// <summary>
+		/// Move the character in a given direction.
+		/// 
+		/// If the magnitude of the movement vector is equal to or greater than one,
+		/// then the character moves at their max speed.
+		/// </summary>
+		/// <param name="direction">Direction to walk in.</param>
+		void Walk(Vector2 direction);
+	}
+
+	public interface IAttack : IBody {
+
+		/// <summary>
+		/// The range of the attack.
+		/// </summary>
+		float AttackRange { get; }
+
+		/// <summary>
+		/// Attack in the current facing direction.
+		/// </summary>
+		void Attack();
+
+		/// <summary>
+		/// Attack in the given direction.
+		/// </summary>
+		/// <param name="direction">The direction to aim the attack in.</param>
+		void Attack(Vector2 direction);
+	}
+}
