ec97fdb16df5a0c15d3ca0792b0372c0b5413b54
Autofire <Autofire@users.noreply.github.com>
2019-06-29 18:59:50 -0700

    Worked more with animations.

---
+++ Assets/scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	public Vector3 speed;
+	public Vector3 loopStartPosition;
+	public float loopWhenYLessThan;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(transform.position.y < loopWhenYLessThan) {
+			transform.position = loopStartPosition;
+		}
+
+		transform.position -= speed * Time.deltaTime;
+    }
+}
