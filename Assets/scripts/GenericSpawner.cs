647b7fbfae1bb59b1668477e58f1bd231502604e
Autofire <Autofire@users.noreply.github.com>
2019-07-02 12:22:03 -0700

    Made it so that the player is spawned correctly.

---
+++ Assets/scripts/GenericSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSpawner : MonoBehaviour
+{
+	public GameObject prefab;
+	public Transform parent;
+
+	private void Start() {
+		Instantiate(prefab, transform.position, prefab.transform.rotation, parent);
+	}
+}
