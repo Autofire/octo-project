56563c562c8ba2565f98dd5d3aa04a8daa2afe58
Autofire <Autofire@users.noreply.github.com>
2019-07-04 14:00:45 -0700

    Created life pool.

---
+++ Assets/scripts/Attacks/LifePool.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using ReachBeyond.VariableObjects;
+
+public class LifePool : MonoBehaviour
+{
+#pragma warning disable CS0649
+	[SerializeField] private IntConstReference maximum;
+	[SerializeField] private IntReference _current;
+#pragma warning restore CS0649
+
+	public int CurrentLife {
+		get {
+			return _current;
+		}
+		set {
+			_current.Value = Mathf.Clamp(value, 0, maximum);
+		}
+	}
+
+
+	public void OnValidate() {
+		// This is a pressure release valve. Otherwise, OnValidate
+		// will run when we're just instantiated which is obviously bad.
+		if(maximum == null || _current == null) {
+			return;
+		}
+
+		if(maximum < 0) {
+			Debug.LogWarning("Max life set below zero... that shouldn't happen.");
+		}
+		else {
+			int oldCurrent = _current;
+			CurrentLife = _current;
+
+			if(oldCurrent != CurrentLife) {
+				Debug.LogWarning("Life total must not be set outside normal range; resetting.");
+			}
+		}
+	}
+
+}
