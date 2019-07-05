925ce817fdcbdc3ca24bfc8b7eb99ca3a6033920
Autofire <Autofire@users.noreply.github.com>
2019-07-05 15:41:17 -0700

    Made the player's health get tracked.
    Updated the spawn area.

---
+++ Assets/scripts/MeterController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ReachBeyond.VariableObjects;
+
+public class MeterController : MonoBehaviour
+{
+	public Slider slider;
+	public IntConstReference value;
+	public IntConstReference maximum;
+
+	public void OnGUI() {
+		slider.value = value / (float) maximum;
+	}
+}
