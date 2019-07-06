54a61fed7278ba50c154c451d330bd407cbd6051
Autofire <Autofire@users.noreply.github.com>
2019-07-05 20:57:59 -0700

    Did visibility code for the ghost.

---
+++ Assets/scripts/Character/Brains/GhostBrain.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using ReachBeyond.VariableObjects;
+using Characters.Bodies.Interfaces;
+
+namespace Characters.Brains {
+
+	public class GhostBrain : MonoBehaviour {
+
+#pragma warning disable CS0649
+		[SerializeField] private GameObjectConstReference huntTarget;
+		[SerializeField] private LayerMask visionMask;
+#pragma warning restore CS0649
+
+		[Space(10)]
+		public IWalk walkComp;
+		public IAttack attackComp;
+
+		private void Awake() {
+			if(walkComp == null) {
+				walkComp = GetComponent<IWalk>();
+			}
+
+			if(attackComp == null) {
+				attackComp = GetComponent<IAttack>();
+			}
+		}
+
+		private void Update() {
+			if(huntTarget.ConstValue == null) {
+				MopeAround();
+			}
+			else {
+				RaycastHit2D hit = Physics2D.Raycast(
+					origin: transform.position,
+					direction: huntTarget.ConstValue.transform.position - transform.position,
+					layerMask: visionMask.value,
+					distance: Mathf.Infinity
+				);
+
+				if(hit.collider != null && hit.transform.gameObject == huntTarget.ConstValue) {
+					// We can see 'em!
+					Debug.Log("Visible");
+				}
+			}
+
+		}
+
+		private void MopeAround() {
+
+		}
+
+	}
+}
