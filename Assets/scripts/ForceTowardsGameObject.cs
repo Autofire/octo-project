a9305af6229b69a3205dee2b94d69f5173ab8206
Autofire <Autofire@users.noreply.github.com>
2020-05-26 10:17:41 -0700

    Made enemies get pushed back when they die.

---
+++ Assets/scripts/ForceTowardsGameObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ReachBeyond.VariableObjects;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ForceTowardsGameObject : MonoBehaviour
+{
+	public GameObjectConstReference obj;
+	public float force;
+
+	private Rigidbody2D rb;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnEnable() {
+		if(obj.ConstValue != null) {
+			Vector2 dir = (obj.ConstValue.transform.position - transform.position).normalized;
+
+			rb.AddForce(dir * force, ForceMode2D.Impulse);
+		}
+	}
+}
