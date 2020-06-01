70c3bcc5c0075dc24641759387f290326367aeda
Autofire <Autofire@users.noreply.github.com>
2020-05-31 23:06:13 -0700

    Reorganized. Refined title screen.

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	public string sceneName;
+	public float minWaitTime;
+
+	public void StartLoad() {
+		StartCoroutine(LoadYourAsyncScene());
+	}
+
+	IEnumerator LoadYourAsyncScene() {
+
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+		yield return new WaitForSecondsRealtime(minWaitTime);
+
+		while(!asyncLoad.isDone) {
+			yield return null;
+		}
+	}
+}
---
+++ Assets/scripts/UI/ToggleGameObjectEnabled.cs
@@ -0,0 +1,22 @@
+﻿using ReachBeyond.VariableObjects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGameObjectEnabled : MonoBehaviour
+{
+	public GameObject[] objects;
+	public float period;
+
+	private float timeToNextToggle = 0f;
+
+	public void Update() {
+		if(Time.time > timeToNextToggle) {
+			timeToNextToggle = Time.time + period;
+
+			foreach(GameObject obj in objects) {
+				obj.SetActive(!obj.activeSelf);
+			}
+		}
+	}
+}
