431cd4615b2b576ebb062b06fc2d84dd106f6211
Autofire <Autofire@users.noreply.github.com>
2020-06-01 17:31:18 -0700

    Created background setter. (Going to import package)

---
+++ Assets/scripts/CameraBackgroundSetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraBackgroundSetter : MonoBehaviour
+{
+	public Color[] colors;
+	public Camera cam;
+
+	private void Awake() {
+		Assert.IsNotNull(cam);
+	}
+
+	public void SetColor(int colorIndex) {
+		cam.backgroundColor = colors[colorIndex];
+	}
+
+}
